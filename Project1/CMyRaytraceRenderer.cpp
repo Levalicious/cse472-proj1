c34277a02e863b02cd8ef8effb41d72702fc1c77
Lev Knoblock <levknoblock@gmail.com>
2021-04-05 18:32:41 -0400

    Antialiasing

--- Project1/CMyRaytraceRenderer.cpp
+++ Project1/CMyRaytraceRenderer.cpp
@@ -116,6 +116,8 @@ bool CMyRaytraceRenderer::RendererEnd()
 	// Extinction coefficient for fog
 	double extCoeff = 0.00001;
 
+	int aaLevel = 0;
+
 	for (int r = 0; r < m_rayimageheight; r++)
 	{
 		for (int c = 0; c < m_rayimagewidth; c++)
@@ -124,100 +126,131 @@ bool CMyRaytraceRenderer::RendererEnd()
 			double colorLight[3] = { 1, 1, 1};
 			float colorTotal[3] = { 0, 0, 0 };
 
-			double x = xmin + (c + 0.5) / m_rayimagewidth * xwid;
-			double y = ymin + (r + 0.5) / m_rayimageheight * yhit;
+			double tavg = 0.f;
 
-
-			// Construct a Ray
-			CRay ray(CGrPoint(0, 0, 0), Normalize3(CGrPoint(x, y, -1, 0)));
-
-			double t;                                   // Will be distance to intersection
-			CGrPoint intersect;                         // Will by x,y,z location of intersection
-			const CRayIntersection::Object* nearest;    // Pointer to intersecting object
-			if (m_intersection.Intersect(ray, 1e20, NULL, nearest, t, intersect))
+			for (int rayx = 0; rayx < 1U << aaLevel; ++rayx) 
 			{
-				// We hit something...
-				// Determine information about the intersection
-				CGrPoint N;
-				CGrMaterial* material;
-				CGrTexture* texture;
-				CGrPoint texcoord;
-
-				m_intersection.IntersectInfo(ray, nearest, t,
-					N, material, texture, texcoord);
-
-				if (material != NULL)
+				for (int rayy = 0; rayy < 1U << aaLevel; ++rayy) 
 				{
-					//calculate shading
-					CGrPoint V = Normalize3(ray.Direction()) * -1;
-					CGrPoint L = Normalize3(m_light - intersect);  //TODO: check if intersect is value to use here?
-					CGrPoint H = Normalize3(V + L);
+					double x = xmin + (c + (double(rayx + 1.f) / double((1U << aaLevel) + 1.f))) / m_rayimagewidth * xwid;
+					double y = ymin + (r + (double(rayy + 1.f) / double((1U << aaLevel) + 1.f))) / m_rayimageheight * yhit;
 
-					double spec = pow(max(Dot3(N, H), 0.0), 2.0);
-					double ambient = .1;
+					// Construct a Ray
+					CRay ray(CGrPoint(0, 0, 0), Normalize3(CGrPoint(x, y, -1, 0)));
 
-					//calculate fog
-					double fogWeight = exp(-t * extCoeff);
-					double fogWeightInv = 1.0 - fogWeight;
-
-					//shadow rays
-					for (int currLightInd = 0; currLightInd < LightCnt(); ++currLightInd) 
+					double t;                                   // Will be distance to intersection
+					CGrPoint intersect;                         // Will by x,y,z location of intersection
+					const CRayIntersection::Object* nearest;    // Pointer to intersecting object
+					if (m_intersection.Intersect(ray, 1e20, NULL, nearest, t, intersect))
 					{
-						const CGrRenderer::Light currLight = GetLight(currLightInd);
+						// We hit something...
+						// Determine information about the intersection
+						CGrPoint N;
+						CGrMaterial* material;
+						CGrTexture* texture;
+						CGrPoint texcoord;
 
-						colorTotal[0] += currLight.m_ambient[0];
-						colorTotal[1] += currLight.m_ambient[1];
-						colorTotal[2] += currLight.m_ambient[2];
+						m_intersection.IntersectInfo(ray, nearest, t,
+							N, material, texture, texcoord);
 
-						CRay shadowRay(intersect, Normalize3(m_mstack.back() * currLight.m_pos));
-						const CRayIntersection::Object* shadowHit;
-						if (!m_intersection.Intersect(shadowRay, 1e20, nearest, shadowHit, t, intersect))
+						if (material != NULL)
 						{
-							colorTotal[0] += currLight.m_diffuse[0] * material->Diffuse(0);
-							colorTotal[1] += currLight.m_diffuse[1] * material->Diffuse(1);
-							colorTotal[2] += currLight.m_diffuse[2] * material->Diffuse(2);
+							//calculate shading
+							CGrPoint V = Normalize3(ray.Direction()) * -1;
+							CGrPoint L = Normalize3(m_light - intersect);  //TODO: check if intersect is value to use here?
+							CGrPoint H = Normalize3(V + L);
 
-							colorTotal[0] += currLight.m_specular[0] * material->Specular(0);
-							colorTotal[1] += currLight.m_specular[1] * material->Specular(1);
-							colorTotal[2] += currLight.m_specular[2] * material->Specular(2);
+							double spec = pow(max(Dot3(N, H), 0.0), 2.0);
+							double ambient = .1;
+
+							//calculate fog
+							tavg += t;
+
+							double colRay[3] = { 0, 0, 0 };
+
+							//shadow rays
+							for (int currLightInd = 0; currLightInd < LightCnt(); ++currLightInd)
+							{
+								const CGrRenderer::Light currLight = GetLight(currLightInd);
+
+								colRay[0] += currLight.m_ambient[0];
+								colRay[1] += currLight.m_ambient[1];
+								colRay[2] += currLight.m_ambient[2];
+
+								CRay shadowRay(intersect, Normalize3(m_mstack.back() * currLight.m_pos));
+								const CRayIntersection::Object* shadowHit;
+								if (!m_intersection.Intersect(shadowRay, 1e20, nearest, shadowHit, t, intersect))
+								{
+									colRay[0] += currLight.m_diffuse[0] * material->Diffuse(0);
+									colRay[1] += currLight.m_diffuse[1] * material->Diffuse(1);
+									colRay[2] += currLight.m_diffuse[2] * material->Diffuse(2);
+
+									colRay[0] += currLight.m_specular[0] * material->Specular(0);
+									colRay[1] += currLight.m_specular[1] * material->Specular(1);
+									colRay[2] += currLight.m_specular[2] * material->Specular(2);
+								}
+							}
+
+							colRay[0] /= LightCnt() * 3;
+							colRay[1] /= LightCnt() * 3;
+							colRay[2] /= LightCnt() * 3;
+
+							colorTotal[0] += colRay[0];
+							colorTotal[1] += colRay[1];
+							colorTotal[2] += colRay[2];
+
+							/*
+
+							if (lightHits == 0) {
+								m_rayimage[r][c * 3 + 0] = BYTE(((material->Diffuse(0) * fogWeight * spec + fogWeightInv * colorFog[0]) + ambient * colorLight[0]) * 255);
+								m_rayimage[r][c * 3 + 1] = BYTE(((material->Diffuse(1) * fogWeight * spec + fogWeightInv * colorFog[1]) + ambient * colorLight[0]) * 255);
+								m_rayimage[r][c * 3 + 2] = BYTE(((material->Diffuse(2) * fogWeight * spec + fogWeightInv * colorFog[2]) + ambient * colorLight[0]) * 255);
+							}
+							else {
+								m_rayimage[r][c * 3 + 0] = BYTE(0);
+								m_rayimage[r][c * 3 + 1] = BYTE(0);
+								m_rayimage[r][c * 3 + 2] = BYTE(0);
+							}
+							*/
+
 						}
+						else 
+						{
+							colorTotal[0] += colorFog[0];
+							colorTotal[1] += colorFog[1];
+							colorTotal[2] += colorFog[2];
+							tavg += 1000.f;
+						}
+
+
+
 					}
-
-					colorTotal[0] /= LightCnt() * 3;
-					colorTotal[1] /= LightCnt() * 3;
-					colorTotal[2] /= LightCnt() * 3;
-					
-
-					m_rayimage[r][c * 3 + 0] = BYTE((colorTotal[0] * fogWeight + fogWeightInv * colorFog[0]) * 255);
-					m_rayimage[r][c * 3 + 1] = BYTE((colorTotal[1] * fogWeight + fogWeightInv * colorFog[1]) * 255);
-					m_rayimage[r][c * 3 + 2] = BYTE((colorTotal[2] * fogWeight + fogWeightInv * colorFog[2]) * 255);
-
-					/*
-
-					if (lightHits == 0) {
-						m_rayimage[r][c * 3 + 0] = BYTE(((material->Diffuse(0) * fogWeight * spec + fogWeightInv * colorFog[0]) + ambient * colorLight[0]) * 255);
-						m_rayimage[r][c * 3 + 1] = BYTE(((material->Diffuse(1) * fogWeight * spec + fogWeightInv * colorFog[1]) + ambient * colorLight[0]) * 255);
-						m_rayimage[r][c * 3 + 2] = BYTE(((material->Diffuse(2) * fogWeight * spec + fogWeightInv * colorFog[2]) + ambient * colorLight[0]) * 255);
+					else
+					{
+						// We hit nothing...
+						colorTotal[0] += colorFog[0];
+						colorTotal[1] += colorFog[1];
+						colorTotal[2] += colorFog[2];
+						tavg += 1000.f;
 					}
-					else {
-						m_rayimage[r][c * 3 + 0] = BYTE(0);
-						m_rayimage[r][c * 3 + 1] = BYTE(0);
-						m_rayimage[r][c * 3 + 2] = BYTE(0);
-					}
-					*/
-					
 				}
-
-
-
 			}
-			else
-			{
-				// We hit nothing...
-				m_rayimage[r][c * 3] = colorFog[0] * 255;
-				m_rayimage[r][c * 3 + 1] = colorFog[1] * 255;
-				m_rayimage[r][c * 3 + 2] = colorFog[2] * 255;
-			}
+
+			colorTotal[0] /= (1U << aaLevel);
+			colorTotal[1] /= (1U << aaLevel);
+			colorTotal[2] /= (1U << aaLevel);
+
+			tavg /= (1U << aaLevel);
+
+			double fog = exp(-tavg * extCoeff);
+
+			double fogInv = 1 - fog;
+
+
+			m_rayimage[r][c * 3 + 0] = BYTE((colorTotal[0] * fog + fogInv * colorFog[0]) * 255);
+			m_rayimage[r][c * 3 + 1] = BYTE((colorTotal[1] * fog + fogInv * colorFog[1]) * 255);
+			m_rayimage[r][c * 3 + 2] = BYTE((colorTotal[2] * fog + fogInv * colorFog[2]) * 255);
+			
 		}
 		if ((r % 50) == 0)
 		{
